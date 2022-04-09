4118161615a5d4e194e09b47eadf76da09aee89b
Irvin <itancio@berkeley.edu>
2022-04-09 12:47:44 -0700

    Implement parser (Juan's version)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include <cctype>
+#include <fstream>
 #include <iostream>
 #include <numeric>
 #include <string>
 #include <vector>
 
+#include <dirent.h>
 #include "sokoban.hpp"
 
 //#include <emscripten/emscripten.h>
@@ -57,5 +60,48 @@ int sokoban_level() {
 
 int main() {
     std::cout << "Hello Emscripten!\n";
-}
 
+    // Open directory
+    // https://www.quora.com/How-do-I-read-all-files-in-a-folder-in-C
+    // https://www.tutorialspoint.com/How-can-I-get-the-list-of-files-in-a-directory-using-C-Cplusplus
+    struct dirent *entry;
+    DIR *dir = opendir("src/levels/");
+   
+    if (dir == NULL) {
+      return;
+    }
+    while ((entry = readdir(dir)) != NULL) {
+        std::cout << entry->d_name << std::endl;
+    }
+    closedir(dir);
+
+
+
+
+    // Read from the text file
+    std::ifstream file("src/hello.txt");
+
+    if (!file.is_open()) {
+        std::cout << "File cannot be open.\n";
+        return 1;
+    }
+    std::string line;
+
+    std::vector<std::string> level;
+    // Read file line by line
+    while (std::getline(file, line)) {
+        if (isalpha(line.at(0))) {
+            break;
+        }
+        level.push_back(line);
+    }
+
+    for (auto &str : level) {
+        std::cout << str << "\n";
+    }
+
+    // Close the file
+    file.close();
+
+    return;
+}
