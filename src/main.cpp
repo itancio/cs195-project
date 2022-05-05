6a6d0ae9d5d0a9d4a01dae2f17641ccefb13f2e7
Irvin <itancio@berkeley.edu>
2022-05-04 18:57:50 -0700

    Fix Parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -21,38 +22,27 @@ static std::string sequence_str;
 static std::vector<std::vector<std::string>> levels;
 
 extern "C" {
+
 void sokoban_initialize() {
     /* Read files from directory */
     const std::string source_path = "src/assets/levels";
-    std::filesystem::path get_levels_dir = std::filesystem::directory_entry(source_path);
-    std::vector<std::filesystem::path> get_map_path; // save path into vector element
+    std::filesystem::path levels_dir = std::filesystem::directory_entry(source_path);
 
-    for (const auto& entry : std::filesystem::directory_iterator(get_levels_dir)) {
-        get_map_path.push_back(entry.path());
-    }
-
-    // opening map files stored in vector directory
-    for (unsigned int i = 0; i < get_map_path.size(); i++) {
-        std::ifstream open_map_file(get_map_path[i].c_str());
-        std::string store_line;
-        std::vector<std::string> store_map;
-
-        if (!open_map_file) {
-            std::cerr << "Cannot open the File : " << get_map_path[i] << std::endl;
-        }
+    for (const auto& entry : std::filesystem::directory_iterator(levels_dir)) {
+        std::ifstream open_map_file(entry.path());
+        std::string line;
+        std::vector<std::string> level;
 
         std::regex valid_elems("[#@$*.+]+");
-        std::regex invalid_elems("[a-zA-z0-9]+");
-        while (std::getline(open_map_file, store_line)) {
-            if (std::regex_search(store_line, valid_elems)) {
-                store_map.push_back(store_line);
+        while (std::getline(open_map_file, line)) {
+            if (std::regex_search(line, valid_elems)) {
+                level.push_back(line);
             }
-            if (std::regex_search(store_line, invalid_elems) and !store_map.empty()) {
+            else if (!level.empty()) {
                 break;
             }
-
         }
-        levels.push_back(store_map);
+        levels.push_back(level);
     };
 
     soko = { levels };
